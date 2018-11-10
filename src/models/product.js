e886e7da2a879fc396feb9f4613b46aa6e66a846
Teslima02 <teslimafeez@gmail.com>
2018-11-10 11:02:57 +0100

    remove mongoosastic

--- src/models/product.js
+++ src/models/product.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 var mongoosePaginate = require('mongoose-paginate');
-import mongoosastic from 'mongoosastic';
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
@@ -10,7 +9,7 @@ const Product = new Schema({
   _categoryId: { type: Schema.Types.ObjectId, ref: 'categories' },
   _createdBy: { type: Schema.Types.ObjectId, ref: 'accounts' },
   _updatedBy: { type: Schema.Types.ObjectId, ref: 'accounts' },
-  productName: { type: String, es_indexed: true },
+  productName: String,
   pieces: Number,
   sellingPrice: Number,
   expiryDate: String,
@@ -30,7 +29,4 @@ const Product = new Schema({
   promoPieces: Number,
 });
 Product.plugin(mongoosePaginate);
-// var esClient = new elasticsearch.Client({ host: 'localhost:9200' });
-// Product.plugin(mongoosastic, {  esClient: esClient });
-Product.plugin(mongoosastic, { hosts: ['127.0.0.1:9200'], bulk: { size: 2800, delay: 100 } });
 export default mongoose.model('products', Product);
