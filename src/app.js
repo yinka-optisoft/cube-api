c98d0db11a787a1518f4b8ab2859cb7fb22d761a
ehizokhai <ehizokhai@gmail.com>
2018-12-14 18:17:50 +0100

    hgfh

--- src/app.js
+++ src/app.js
@@ -125,8 +125,8 @@ passport.deserializeUser(Account.deserializeUser());
 
 try {
 
-  mongoose.connect('mongodb://localhost/cube');
-  // mongoose.connect('mongodb://cube_master:JustSomeCleanup@cube700-shard-00-00-52rkf.mongodb.net:27017,cube700-shard-00-01-52rkf.mongodb.net:27017,cube700-shard-00-02-52rkf.mongodb.net:27017/test?ssl=true&replicaSet=cube700-shard-0&authSource=admin');
+ // mongoose.connect('mongodb://localhost/cube');
+ mongoose.connect('mongodb://cube_master:JustSomeCleanup@cube700-shard-00-00-52rkf.mongodb.net:27017,cube700-shard-00-01-52rkf.mongodb.net:27017,cube700-shard-00-02-52rkf.mongodb.net:27017/test?ssl=true&replicaSet=cube700-shard-0&authSource=admin');
 
 } catch (e) {
   throw e;
